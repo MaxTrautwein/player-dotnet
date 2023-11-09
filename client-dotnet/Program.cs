d7125fd9b24604aeffa9fceaa57820bd27ceca01
Andreas Heinrich <github@heinrian.com>
2023-11-09 22:17:16 +0100

    move code in first directory level

--- client-dotnet/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapPost("/", (System.Text.Json.JsonElement test) =>
-{
-    return "{\"bet\": \"" + Strategy.decide(Newtonsoft.Json.JsonConvert.DeserializeObject<Table>(test.ToString())).bet + "\"}";
-});
-
-app.Run();
--- logic/strategy.cs
+++ logic/strategy.cs
@@ -1,6 +1,6 @@
 public class Strategy
 {
-    public static Bet decide(Table table) {
+    public static Bet decide(Table? table) {
         // TODO: Add Poker Logic Here... :)
         
         return new Bet(0);
