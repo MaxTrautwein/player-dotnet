1d349a03b4e73724bac131c255797dcf169ff072
Max Trautwein <trautwein.max@gmail.com>
2023-11-16 21:41:06 +0100

    log test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+app.Logger.Log(LogLevel.Error,"###########");
+
+
+
 app.MapPost("/", (System.Text.Json.JsonElement body) => "{\"bet\": \"" + Strategy.Decide(Newtonsoft.Json.JsonConvert.DeserializeObject<Table>(body.ToString())).bet + "\"}");
 
 app.MapGet("/", () => "Player C#/.net");
