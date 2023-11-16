f9f2db86c44a33d0807e4b328e795c1cb894c114
Max Trautwein <trautwein.max@gmail.com>
2023-11-16 21:28:25 +0100

    debug

--- logic/strategy.cs
+++ logic/strategy.cs
@@ -2,19 +2,31 @@ using client_dotnet.models;
 
 public class Strategy
 {
+    
+    
     public static Bet Decide(Table? table) {
         // fold on null data
-        if (table is null) return new Bet(0);
+        if (table is null)
+        {
+            Console.WriteLine("Table Null");
+            return new Bet(0);
+        }
         
         var ourPlayer = table.players[table.activePlayer];
 
         if (HasSimilarRanks(ourPlayer) == false && HasSimilarSuits(ourPlayer) == false)
         {
+            Console.WriteLine("bet null - bad Cards");
             return new Bet(0);
+            
         }
+        
+        Console.WriteLine("bet minimum");
         return new Bet(table.minimumBet);
     }
-    
+
+
+    public static Bet Fold => new Bet(0);
     
     
     static bool HasSimilarRanks(Player player)
