b59251bba46c7d218cb0c6323a17e09886fbab8e
Ayleen Weiss <ayweit00@hs-esslingen.de>
2023-11-16 20:21:36 +0100

    Add functions to check for similar rank and similar suit

--- logic/strategy.cs
+++ logic/strategy.cs
@@ -12,8 +12,43 @@ public class Strategy
         return new Bet(table.minimumBet);
     }
     
-    
-    
+    public bool HasSimilarRanks(Player player)
+    {
+        if (player?.cards == null || player.cards.Length < 2)
+            return false;
+
+        for (int i = 0; i < player.cards.Length; i++)
+        {
+            for (int j = i + 1; j < player.cards.Length; j++)
+            {
+                if (player.cards[i].rank == player.cards[j].rank)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public bool HasSimilarSuits(Player player)
+    {
+        if (player?.cards == null || player.cards.Length < 2)
+            return false;
+
+        for (int i = 0; i < player.cards.Length; i++)
+        {
+            for (int j = i + 1; j < player.cards.Length; j++)
+            {
+                if (player.cards[i].suit == player.cards[j].suit)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     
 }
 
