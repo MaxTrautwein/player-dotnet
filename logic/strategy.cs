63792313cad26d15f38b1597b6e6a3e05706a969
Ayleen Weiss <ayweit00@hs-esslingen.de>
2023-11-16 21:47:11 +0100

    Check for desired combinations and go all in if true

--- logic/strategy.cs
+++ logic/strategy.cs
@@ -9,10 +9,22 @@ public class Strategy
         
         var ourPlayer = table.players[table.activePlayer];
 
+        // fold if no pair (suits or ranks)
         if (GameUtils.HasSimilarRanks(ourPlayer) == false && GameUtils.HasSimilarSuits(ourPlayer) == false)
         {
             return new Bet(0);
         }
+        
+        // only in round 1 (player have 2 cards, no cards on table)
+        if (table.communityCards == null || table.communityCards.Length == 0)
+        {
+            if (GameUtils.HasSimilarSuits(ourPlayer) && GameUtils.HasDesiredCombination(ourPlayer))
+            {
+                // all in
+                return new Bet(ourPlayer.stack);
+            }
+        }
+        
         return new Bet(table.minimumBet);
     }
     
--- logic/utils.cs
+++ logic/utils.cs
@@ -1,7 +1,56 @@
 namespace client_dotnet.logic;
 
+public static class CardCombinations
+{
+    public static HashSet<Tuple<Rank, Rank>> GetDesiredCombinations()
+    {
+        return new HashSet<Tuple<Rank, Rank>>
+        {
+            new Tuple<Rank, Rank>(Rank.A, Rank.K),
+            new Tuple<Rank, Rank>(Rank.A, Rank.Q),
+            new Tuple<Rank, Rank>(Rank.A, Rank.J),
+            new Tuple<Rank, Rank>(Rank.A, Rank._10),
+            new Tuple<Rank, Rank>(Rank.K, Rank.Q),
+            new Tuple<Rank, Rank>(Rank.K, Rank.J),
+            new Tuple<Rank, Rank>(Rank.K, Rank._10),
+            new Tuple<Rank, Rank>(Rank.Q, Rank.J),
+            new Tuple<Rank, Rank>(Rank.Q, Rank._10),
+            new Tuple<Rank, Rank>(Rank.J, Rank._10),
+            new Tuple<Rank, Rank>(Rank.J, Rank._9),
+            new Tuple<Rank, Rank>(Rank._10, Rank._9),
+        };
+    }
+}
+
 public class GameUtils
 {
+    
+    public static bool HasDesiredCombination(Player player)
+    {
+        var desiredCombinations = CardCombinations.GetDesiredCombinations();
+
+        if (player?.cards == null || player.cards.Length < 2)
+            return false;
+
+        for (int i = 0; i < player.cards.Length; i++)
+        {
+            for (int j = i + 1; j < player.cards.Length; j++)
+            {
+                var pair1 = new Tuple<Rank, Rank>(player.cards[i].rank, player.cards[j].rank);
+                var pair2 = new Tuple<Rank, Rank>(player.cards[j].rank, player.cards[i].rank);
+            
+                if (desiredCombinations.Contains(pair1) || desiredCombinations.Contains(pair2))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    
     public static bool HasSimilarRanks(Player player)
     {
         if (player?.cards == null || player.cards.Length < 2)
