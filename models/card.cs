fa6289a7a3c652548df35760c668e0e45686b0e9
Philipp Lichtmannegger <info@lichtmannegger>
2023-11-18 15:09:39 +0100

    fixed enum problem in public int Val() in card.cs

--- models/card.cs
+++ models/card.cs
@@ -2,42 +2,52 @@ public class Card
 {
     public Rank rank;
     public Suit suit;
-    
+
     public int Val()
     {
         switch (this.rank)
         {
-            case Rank.A:
+            case (Rank)'A':
                 return 14;
-            case Rank.K:
+            
+            case (Rank)'K':
                 return 13;
-            case Rank.Q:
+            
+            case (Rank)'Q':
                 return 12;
-            case Rank.J:
+            
+            case (Rank)'J':
                 return 11;
-            case Rank._10:
+            
+            case (Rank)'T':
                 return 10;
-            case Rank._9:
+            
+            case (Rank)9:
                 return 9;
-            case Rank._8:
+            
+            case (Rank)8:
                 return 8;
-            case Rank._7:
+            
+            case (Rank)7:
                 return 7;
-            case Rank._6:
+            
+            case (Rank)6:
                 return 6;
-            case Rank._5:
+            
+            case (Rank)5:
                 return 5;
-            case Rank._4:
+            
+            case (Rank)4:
                 return 4;
-            case Rank._3:
+            
+            case (Rank)3:
                 return 3;
-            case Rank._2:
+            
+            case (Rank)2:
                 return 2;
+            
             default:
                 throw new ArgumentOutOfRangeException(nameof(rank), rank, null);
         }
     }
-
-    
 }
-
